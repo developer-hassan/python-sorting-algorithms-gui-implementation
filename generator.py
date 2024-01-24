284b7fb1dfea88c3ddaeadd19894199301d31da4
Muneeb Shafi <muneebshafi527@gmail.com>
2024-01-24 16:03:43 +0500

    Random Array Update

--- generator.py
+++ generator.py
@@ -7,17 +7,20 @@ class Generator:
     Generates the random array for user in given range
     '''
 
-    def create_random_array(array_size, lower_bound, upper_bound):
-        '''
-        create array from the user inputted values
-        '''
+    # def create_random_array(array_size, lower_bound, upper_bound):
+    #     '''
+    #     create array from the user inputted values
+    #     '''
 
-        # Initializing the random array
-        random_array = []
+    #     # Initializing the random array
+    #     random_array = []
 
-        for i in range(array_size):
-            # Populating array with random integer in given range
-            random_array.append(random.randint(lower_bound, upper_bound))
+    #     for i in range(array_size):
+    #         # Populating array with random integer in given range
+    #         random_array.append(random.randint(lower_bound, upper_bound))
 
-        # returning the populated array
-        return random_array
+    #     # returning the populated array
+    #     return random_array
+
+    def create_random_array(size, min_limit, max_limit):
+        return [random.randint(min_limit, max_limit) for _ in range(size)]
